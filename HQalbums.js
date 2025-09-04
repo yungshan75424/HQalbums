c90465545d10a2b7fce14b530d06ff8335c7f05a
YuXiang <53873130+yungshan75424@users.noreply.github.com>
2025-09-04 01:27:37 +0000

    HQ照片

---
+++ HQalbums.js
@@ -0,0 +1,234 @@
+  const albums = 
+  [
+  {
+    "year": "2025",
+    "date": "2025.04.30",
+    "title": "HQ-2025.04.30 2025凱斯偏鄉SDGs永續發展繪本推廣-石磊國小",
+    "link": "https://photos.google.com/share/AF1QipPyhKBS9F3kXxNozoDbLu3cl0eGsH3VeXR7Kv29QIUlXN7IG_4lqeRsZH4UF92puQ?key=N2JaeERTNEhoRVBPTmRaV0pJZ2s2NU9VUGtIcTZ3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczPf7gkscVpH9_fwdMsuau4cqhXAE7nRYEWD7jtifpJwlQGnW9gtVQOCYnGcpqwj2QWHrQ8quiKfucwOd_DFZX9NqK1LjkNHV64xREhlZpXZsyxkUxFLYLYI4kUHsaPvT2oGl14uyS1M-AqbBj2NEI6FQA=w1379-h919-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2025",
+    "date": "2025.04.22",
+    "title": "HQ-2025.04.22 凱斯英語4月教師訓練",
+    "link": "https://photos.google.com/share/AF1QipN_15nyG6or8ka2X8vSsgLgzaZTrs5GFPyHWb_D2_83VrXYwaNVcuYqay-NBx7Rgw?key=c3VtX1o0R2ttNTh1bXVvVHZPS0dKOEJlT2ZRV1l3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMV0UplhA7bVEWmioor_oYCuvzv_EhJOfWUkz5mb-UWN_ZYO7DBKqEQ3wAhY-wE7iMecjioapcTIdst6lAKV6f8qSGjN_65-TO6ZaimU6xw3g7X1MCymitcWjkykERrVGZoZP7SaX3Yn6qTJXsN1O4AiQ=w1418-h945-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2025",
+    "date": "2025.03.25",
+    "title": "HQ-2025.03.25凱斯英語3月教師訓練",
+    "link": "https://photos.google.com/share/AF1QipOp7s10aBwkUmNooHDdjxOBWbcTRwAJdUEzM0IBbeYyN4v-rhhGdJ_-RYUTogKvvw?key=WXhmZ3RKSEZlaFl0aWdIUzdVSFVzcGRFQWltR0NB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMIQUDNHovO1h_9uRWAq7JOPQ6muhjEiqZkX5X9AQ2lbl_uqxOkYEmn76PTtcx3Clb250ANjHRNUr-E3yEzCb7SyU41iySiEONTA1hzRRbznVDC0jfFffHfjkkLaPBO99vQAmbr1johWzGnOw6NpdfRCg=w1379-h919-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2025",
+    "date": "2025.03.12",
+    "title": "HQ-2025.03.12 2025凱斯偏鄉SDGs永續發展繪本推廣-瑞亭國小",
+    "link": "https://photos.google.com/share/AF1QipNsj22eD6dQQgcn-ZTuXmI25sn7nJk8kuUgHbuWObFTdf2w6jYKgZaocEQZoTJgGg?key=ZkFXTGJsN0hBS09DRE16ekJ0TU5OOG5KNk01eDNR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMIAXYRJKNbB4zuO8eQIz0_UNAvUxeKEUv8apSJvxtOuIBejLrh4sJcOrNCIC6Mk6emK3GLFPV579E7z-dJBYA5LpFftCcEXmTXVVUZ1FBHHuXsILmFqs45t9Q1RIu6zq8QD5UnX_B3oKUWESex9VFyzw=w1418-h945-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2025",
+    "date": "2025.01.18",
+    "title": "HQ-2025.01.18 凱斯教育尾牙- 凱斯復古搖滾夜",
+    "link": "https://photos.google.com/share/AF1QipNY9oNe7qKjB2kzeFzZ9_QBh_OLYTanXE1Ls1Fk8NauiG3B4dcphJXxnmoK5Qzzyw?key=NFNFcFFnMW5zOXVzdVpDdjBpVjFNYU02R0NVelV3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczN050uIh1uJhsUwtOrOmoElK29xvc7janHf4xxUC3EFcPMQUPEzT7CD2F-ByTqP_fyFuVonhPXjrjd0fF1BZR5VBuNVNVTC7B7zSw9Z_icfXRrnd2h-Vh8YCh0vFf3OXQ8VhcUabz-eAoVDdqqcKa3J4A=w1379-h919-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2024",
+    "date": "2024.12.23",
+    "title": "HQ-2024.12.23 【2024耶誕送暖】凱斯孩子的愛心傳遞，點亮樂山 - 英語分班篇",
+    "link": "https://photos.google.com/u/4/share/AF1QipOKkXBEUk5owQTUGwUHNpT94FHVCxBBh4dGTjDHQi_MFJrowiRLvYGnOfR69OcSeg?key=ODZsaHE4SmdPTDZaRWlpX09BLXRMaG1Od1dyTkdR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNwxl6Ml3Cr1bzB7cxadGL_Vxbkxt1ST2PtszfPU5Sa9CUxJkbcgJ0_AxEgtsPAEQu-Z61_VcWUjefqoiGzVV9V_TKmD9BDLjfuZAWssW-1-EwsfxC8wIYr2x1zhqTPKo-uIIKLffJqvONMqWGaGhGgGw=w1260-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.12.23",
+    "title": "HQ-2024.12.23 【2024耶誕送暖】凱斯孩子的愛心傳遞，點亮樂山 - 幼兒園篇",
+    "link": "https://photos.google.com/u/4/share/AF1QipMDDe2L1BLcyPAzkI_o_-oCFKfD-K5NwbI07Sx9LjzvXmWSgB0ozCphqruBfSzs0g?key=SXBMNzhUYWhBb3RGc0tFX2h3MjFxVy1maTdpVXpB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNHnW2p1aXTjugdlGuRrNNrGK1dnNU2n2o2LpLA5Mcpx2fr50JDjNxvJ3vDIZAY-uhGgb39n0nF8Aj4Qo7EKoBpExM29uvj-HNQ3irTGcY_uDNoc6mwjlNxwB0iFscTNK3SPAFKyAnmeus7AbfgZ9ffuQ=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.12.17",
+    "title": "HQ-2024.12.17 凱斯英語12月教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipMruQk553h4itrN943wUvm472zXwigtJktC8F_YzO_bkI_-ROPX8C_4tU1gq82Ftw?key=aEQxSEkxVUlrc1BIeUR4NFdBbDhYXy1iQ1F3NlhR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczPw31-I2gINqAp-AocvtAMCNXPGQSAv8FWO2TVGJkiPO4x_1oAUTDsh0vfwyrmD-339xinCuoi7AQDj7fNeUdB0Tk4ClqbsQaHfpgIu1rfb4x3DmQCVRD4HBqlpAjfwRfN4axARMQHqCi61UGByeLojWw=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.12.08",
+    "title": "HQ-2024.12.08 凱斯慈善耶誕園遊會",
+    "link": "https://photos.google.com/u/4/share/AF1QipMIEZeOSm7UP4TCN0efbkQNzL_q_rZgzTIWJS-UrroT74VErhwzI4vZFQRLB3gImQ?key=YVBjX3pqaXU3blhOWTczS3Z5cUJMbGNuaGtNT2tn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMVhs_kIhU3rUZAI7m4pWSmZpjvyWAaOtUR8CGh5XHqJSHrPEPKCzOtvn0WRiRdnBWo7gd5G-KWB_s3vfS-8TZBbmdAUOxjLHs8P5Q5lDVAJu5bcZ00iS7bo0EWsJ4rpGUNf38zY3tb98bpPzTe9F7OLA=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.11.28",
+    "title": "HQ-2024.11.28 2024凱斯偏鄉SDGs永續發展繪本推廣-尖石國小",
+    "link": "https://photos.google.com/u/4/share/AF1QipMTucpGewCGlG6G5ehd8b4prnRSX-pIZodBmFjDVsVZwSDD7FRDy6_U-0LBhJjPag?key=TUtOOFYzSWQzZ2h4YjBJS3U0UTZxcFhLTDRyNHVn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczORpCnl-aPKspYO5xLN74q8YPJtv_TLFUtp_9nqiti_20yRjdgHBUE9aI3RKQ8W1OIOqhjgOLUmwpLC3dauTwa5Tol_io5EDnh3WtYmCVHKd6L6iESVFx8HNsvGXXoX3FsY1lSpZbkhc7lOAgqv58xlkg=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.11.12",
+    "title": "HQ-2024.11.12 凱斯英語11月教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipPTODQY_btoiIisNGrRStrExQ1B_vbkcD-A4sEzCEVOk_HLBIjAlth7R-Dr5UvwvA?key=MzB3UnowWWdrMHFSV0Y2RFk3R05nMU5OV052ekdR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczPnBYlKsTGhp0aENbqnEXTvKTaF7BPpd6VKkGAw_ajOHGAR0SszCdxGmsTqd2wn0lCNpEikv3rGNH4flBL_bBXBppk9paHDq8R4miDcMcQw-OwZ1lAuB5qbgLok0cXYdrW1KUnitnbbh7C2oN3WGfrDBQ=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.10.24",
+    "title": "HQ-2024.10.24 2024凱斯偏鄉SDGs永續發展繪本推廣-鼻頭國小",
+    "link": "https://photos.google.com/u/4/share/AF1QipPg-1-Jh3ihBn56ZCARa69Ix_SqCNwrRb04zGAJcKhEzY7u-5TJvolv7EkL5wHDuw?key=RjJqdllTanBUN293TXJad3VoZXgyN2t4UzJWbUlB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOZjw63G9dTWzDfTaEqBsSE71WGuteQNciBAeUWjo8By8vdu11Jmhm8xRycJo5ouWsCr1qAi4xBDPGnEKveWW_1OuYi6CgfqfCfOb7WHYirXXfAzk4iiM8JVc0VyA7Hzu_UwTpe4Cs-MXkPVHfe-u9vIg=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.10.22",
+    "title": "HQ-2024.10.22 凱斯英語10月教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipM6-kbfEXbNykuqZ89OV_coT7tfXAZ0KOzs2rkoVsQv6d2Ma87dYbwJkLxlZizSCw?key=bTBmbHkzRmMtc0RKTzJTdzlMRDlhcmZ4UTAxX1R3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOcwZIfDo9VXVvfmPLH3sGqzcX2nTS_LUHreQ8PlTUnQPmJS07XXdRHCATdRrTQje_rOGxrw-WfNknDNqoJIuY2GVpGD0BRdDt0-dRVLJgZOEsQDWsQMv1iZjrTyXjECyJz8uL7GnxtkH1THONK0kiWYA=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.09.27",
+    "title": "HQ-2024.09.27 2024凱斯偏鄉SDGs永續發展繪本推廣-九份國小",
+    "link": "https://photos.google.com/u/4/share/AF1QipMzZV46F8H6u7dMwckXdGK2Zvn1VgSa7DemMQ1RFolXI4IL00madxhbs_JiaJeLAA?key=N3p2NWlUSkxGYmZDTFN6a3ZmSmc0MWpNYkMzQWxn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczO9oXDzCDVcww2p9yG-JB01gAbRg_ai-Td0_FwF8uO5pbuhVwr387WsrHVUOzJg4goQi9Ip6ceWEAesHaDW358aneSUUYUUoRyDy5SylmrwJjFz2kjcpV-OfCp-MVt9yhb965Mghc_Q0CXXeETye-tbAA=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.09.10",
+    "title": "HQ-2024.09.10 凱斯總部英語總教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipO_6-o3qQXQyRkkGt42XGmg4RcGlh_JwpqreY6FAiaFHiLHaS3OyKFlOBBLcFq55g?key=TGpnSFg1cUtYRmswRkhFOXQ3MGZnUXk3SVZta0F3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczPnYIDOvjZVmxhJD7v1SRyJObMo3X0GCJ1g0N7-8chhCWLKaQ8bbvctovT-jXsiGYGmZd3aBJXPAfzxyE25ziVDtE1lK4JUru7pZQ14NAFgxglLLO58XN-lAjorKBmD4LKTGTwg4VFxpAhlO1rbqJbuTw=w1417-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.08.21",
+    "title": "HQ-2024.08.21 凱斯英語新進教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipODKptKMW30phQMPair1M2B-4SGRpw7xM3V_vBeOa1JAy-OZqIMRlYzNjyj2D0gag?key=dEIycWJRWUNtaGhHSE43R0dCVGkzYUZ0QllJU1dn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOE9HUio_aeE2b37p2j3LxjgcrpsBb-NKu2N9AulxOqwCg9jgKCqvBw6TXirYkbSQ0N89x9Ys-iuOByIT0uUMqKqNhFy39E9sTXXeEE117dZ6MvF-Lgq_ikHEQKTR4M1QAHTyqtTtogA90L8pISzjgcAQ=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.07.29",
+    "title": "HQ-2024.07.29-30 凱斯總部8月幼兒園新進教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipMOcQnrAcfS6NYm0mbsWUk5l3Uir1bBuRXkBgwJ15oiM294MNGgLHqiaEyxRdRLbw?key=c3lMMmN0emFwS1FfdU9KS3FOUnB6UFg2cnhOcDFR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMfyAgjaHE6Gw37Nak0Nn8kR5P6QfOyF8cxTpnNhnWgxQlfJ094iTNQBlgwCC6nqkh2w4OtSw1zC1OdgtEt62dcitH41MtVqcJPb0pvI2kBNwzs00STsFU8DOwG-gnyS2-33aQbtD7MD4hUGq6_85dG9A=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.07.12",
+    "title": "HQ-2024.07.12 2024凱斯教育機構所屬幼兒園畢業典禮暨遊藝會",
+    "link": "https://photos.google.com/u/4/share/AF1QipNgR3oHD7IZz0Ek-s2HwLCPI00hEPOYrbXkgk_TbAQd7GYWgwDI-cub2COeE6Q-kQ?key=TlBnelhpNGc5VHYyZGZJNEU4Nm9yTjRuVFo4MURR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczPGzVMUvs50Z1ge2nbfFyCzLznCgSMiqVA07T9kK2H83DuuGpwz57C5JfENq_3OHPUnYAizg45cUBtbpEqzab7e2xDzG9pxUv4Nz3iDAosy-VkV5nrPZ4aWixYRhL-eQ28RECUlrvPq5xaIEDYRoKIezg=w1418-h945-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.06.18",
+    "title": "HQ-2024.06.18 凱斯總部6月教師訓練 ",
+    "link": "https://photos.google.com/u/4/share/AF1QipMotnv26rFEz1Npn8LtMogoP2fEZt8aKAFy8pcIHbUGTpptA4b5hWDeOWw5oPwUuw?key=bUdOdGxBLTE2WGZpNHBoRWNXRzl3OVBCemttMG1R",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczO6__WESaIBRHWsxOrbzx9NetLNX3QykqhjH47trf4eB77BzSESPg4xAkAzYa32n5d-OjoitO3OWol6vTdGjiNw1m36MdwBuz_dlXLDOdFsKSNGGuppVoMIUSoydTXF-Lr68yBRgzK2Vq3EQZHvg-IbZg=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.06.02",
+    "title": "HQ-2024.06.02 【Cat's X SDGs Fair】2024 SDGs Fair ",
+    "link": "https://photos.google.com/u/4/share/AF1QipOANyUxBtb3YcVAt41k6cFIspX2-Gl_VBNozQFtJFZW7-RxCuIW0ZdRuah6zHYuvg?key=LV9mWk8zSG96RzNvT1FDenhHMUlfcDJmX0pLSWtB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOITBCq5kIEqhCxPUpjPAqY_39aJmDYJzlRvPD7MhiJLe8bwzIZBIHbpvFI-5QPTsCwo45H0xRsd33LPe5dEYKqKhPv7A-MSlas3EsOkXCos-YlPgrNCoOFdytfmTY-LCqxD_mBQmUbA9d-8Iwxp5n9Rg=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.05.21",
+    "title": "HQ-2024.05.21 凱斯總部5月教師訓練 ",
+    "link": "https://photos.google.com/u/4/share/AF1QipMAV1OWbqPksWnYq-WFvXntpQARw2W6ZUWm9sWPnxRYNDZPEYDSzMfaMX4Gs4gWSg?key=ZTViSzB5dTVIMFNrZldyeGwtS1g3X1V1NHM0V0pB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOD5zhSYzfFrdmnDLRJMvD2d5DpK0vqjZd38HgEqzaYH2Ky7hOBHn85tQlSt0PIas8rfmrPo51hAf5y-x1Y1LWvUI63Wf6eyJlP9txWsSSiewziVvAh-moN33eKGM00JqDYtGYMhs6zESnHHJ-iRvM-eQ=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.05.13",
+    "title": "HQ-2024.05.13 2024凱斯偏鄉SDGs永續發展繪本推廣-石磊國小",
+    "link": "https://photos.google.com/u/4/share/AF1QipPQCahoESqQbFxFyqwXBAJJ_PVuWLrwUeCiiT49UFjvuH5suIgxMYaxguI5dyR2qg?key=a2pSeWp3OVl3VlhZakhEcnlIQ0VnclpzamtKSHpB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNn3zO467L7ePtAeBSscTOfejNXijToW6up9ypmfsmMBdyx2uyTMqwlo9xqoqUWnvaFwGXWWC_lWCLYEjnmmAKOJb0kVL5vmX-RnlXj4WiixmaSGcYsIE1sG4SqZKanZyS89J8Svhwc8fkCuFEQaKLJ6w=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.05.08",
+    "title": "HQ-2024.05.08 【凱斯教育Ｘ2024輔仁大學】2024校園就業博覽會 ",
+    "link": "https://photos.google.com/u/4/share/AF1QipOIxvq5DJYqH0_Px-p_gByV7ZqYS9EvukFiOgvwStoDuciqRm7U5tCvRIlTAlpfwA?key=N2xBNzFmQXRkemI2dmFMdkFROHNvQXc1NHN4d2d3",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczP2Y_HPnbZrvuyG4ENAzmXLwk2XH9ugJwfYHbNQ4YoKqYDDdzwijjhlEMPymTsVH-gPyBIL3BWsmAdjJ7wiMcevvxf0sRfT8HHHMpcd7YZpY2mq6jHOIamYnSGDZ70F5_pg7Oi7iID95Ja4ZkLuDOx54A=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.04.30",
+    "title": "HQ-2024.04.30 性騷擾防治教育訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipO6qBj2w3djNgaoIwJaEgNaoVwcuAqMhXjLwSXgcpTX-o-Pj1TI-rsd8fOPVgLJrg?key=eV9tYkpGSGJMMF9SVU96NTktTDN6WV9DU25USnZB",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMleIwF9UesUdozU3Ero987eLQz9olfk_VG0_GKYc2eF4QusCZU5mTyG_LRivEV47RaZwptRwrLqp7vDrW5EzbMhFBMMxUNoZ6Vd7QglZQhY0mjrToMP3BJu8m91sK9uEHoBTq20i7xkfeHRxeSM-vPhQ=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.04.29",
+    "title": "HQ-2024.04.29 2024凱斯偏鄉SDGs永續發展繪本推廣-沙坑國小",
+    "link": "https://photos.google.com/u/4/share/AF1QipNHU3_FphMCoJfz1lIl2H-L19qut3htaKdWFBYn9C1krcslY3FxdtrvoFi9t5v_7A?key=cUlzQlZGX1ZZVUdKZ1hRaWQySjdsTTh1ODRRdW9B",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNsFnVYa2b-kk87XKlkmZJ8xczPhdOjlIa0fjkHxeK1g2rzodr3IDJQfzme230gxijCjSH6z1eDtelifkbgllXJdVDIR-ymBN7IuPqK4iAeEu7az386FHIfB0m6mCotlWJTrqFhAQOYG8u-bjncSh_kOA=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.04.25",
+    "title": "HQ-2024.04.25 凱斯總部4月教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipO2zQIHfVhk2wN1Wo4uSyjIBR8lo7vfi63OnorsRXcfu-FOgDVcxK8Dgoj0Bwc0IA?key=cFlIR2NyeDJDeWhHb3lMMnBXUWJPYmlWSkFTcGRn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOISxp1mEKc-vHvydVcA2pvxTtEfw8oIxnGmB8WYFvNUp4-jhbOLAsHmjRUQiJ5ETBDM9ygALLHzPDgomxULmiERBa8PZJOimhpGPdY4bsGdhkmonP2lJYnA9KFDFVolLbToMpDbKasUVQBcxhsAJwppA=w1680-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.03.28",
+    "title": "HQ-2024.03.28 2024凱斯總部3月行政訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipPWk-gZR_3xDHPxJcPACcSkmjRkKCsSh7-aPHNoTrdwvSbMx8R1nWs5lFK3y0SBCQ?key=cHZSRHB2M2h5SElWU0x0a3ZJcU9aNVBWSVZuS193",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOpcSn9NdftzAdMBJ9ZHxptRh2docoDWYZz_9pnDmEBdDk1Aj7pfcdDpLRWvRDxYJp55gdMShrTKXy1SK4mryA5TG-o850XIxto_lDJE0ZaC1vK8wvmMUpPXUAIUPgc2pD6f98nO1CduGGkZPd_A5B3sg=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.03.26",
+    "title": "HQ-2024.03.26 凱斯總部3月教師訓練 ",
+    "link": "https://photos.google.com/u/4/share/AF1QipO7jSRU7sRnyeE9-6rhF9IgdmCF5ogouWx4dghNj9LI322zwER5C78k_qD_7FHEVA?key=cXhXLWwyb2o1OHQtT1lzNHNqSVB0NDc3dF8zODRR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczOCadVY3oR4i7wRJHttXb9vUovwRHW090mLftYVbOd9UlheSc_vwxNgNihOnt3Ez4VZeAaECD4dwq8QwQt3G1ThdG7YhjeXyuBQtw0I-13UGlWRt3fMkgvfJRekZpcPrtUAp0aPjzuVPoT6FQcFTQHinA=w1891-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.03.25",
+    "title": "HQ-2024.03.25 2024凱斯偏鄉SDGs永續發展繪本推廣-瑞亭國小",
+    "link": "https://photos.google.com/share/AF1QipNsj22eD6dQQgcn-ZTuXmI25sn7nJk8kuUgHbuWObFTdf2w6jYKgZaocEQZoTJgGg?key=ZkFXTGJsN0hBS09DRE16ekJ0TU5OOG5KNk01eDNR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMIAXYRJKNbB4zuO8eQIz0_UNAvUxeKEUv8apSJvxtOuIBejLrh4sJcOrNCIC6Mk6emK3GLFPV579E7z-dJBYA5LpFftCcEXmTXVVUZ1FBHHuXsILmFqs45t9Q1RIu6zq8QD5UnX_B3oKUWESex9VFyzw=w1379-h919-s-no-gm?authuser=0"
+  },
+  {
+    "year": "2024",
+    "date": "2024.03.07",
+    "title": "HQ-2024.03.07 凱斯總部3月學務訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipMDwG8NOPkZYVC7ReskTMVJJQleQn3x9NYnSwttbx_l3jG4yV63rhNcpKjgmHQ1tA?key=RVZ2R1NfYWJSNFJid05pdkZQYmxKQzNTUlN2MW9n",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNkeP-wJhokOQOrQR8MoSDDrB8u7mYfAhwpAQqj2S4-Fk6qiGVRfN0Vs3PjoAs34tPYhXUW6AqHE0pKharjPWT9IL5ZmSUL-SIr4NvhM-ThilHPJJ9OZeL7I6H66Hmw3GflBKTpDBh77J30947OKiJBUA=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.03.02",
+    "title": "HQ-2024.03.02 凱斯教育機構春酒 — 環遊世界",
+    "link": "https://photos.google.com/u/4/share/AF1QipNFWhU6x_5FKE-8PKt5CpZLX_GVVNvtoUfl4euXa70BaHe56Kv7sTTZNiKlRZ6-0A?key=QTRFandLUUVraDAtR2Jad2U3Nm5MNjVDTGhneFNR",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczP7mf5AHF95vrJmNZaXQVj0qSJS5OnSwulBMqGsYtuxFHbc4IpJZmw_362F_yNdw1V1a8SgbY5Bzsf_6pHXjz5LcAUEI1o4KjWQ-47BsHwE5e4Fd_7P-Z5DCIf8iDl40aaJpnXa1SrJWsgmUr2dntqmyw=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.02.15",
+    "title": "HQ-2024.02.15 凱斯總部2月教師訓練 ",
+    "link": "https://photos.google.com/u/4/share/AF1QipN5YmKd0PpGbHbRggsDLKRDxWMsSdF9jGor0NAe7YZfE3wVgMTqLjJm5PfXrfiZbA?key=MEtoRHlreDQtaFhXLXQxbVRManRoV0tLY3c2YmJn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczNE9zdlrhRN4-3UMb1oga5uW0bb2_zuULnDYNnFTtPmrorY5Iv7dEvIvBapZYNryke3kNLBFjmuiVKoHjE_wFhGAteg_QPNM8zEQKu04ZYW0C5rT3j2La9pO693CI5sxhKAHs_4dDwV9IT-qKO2bXTCKg=w1890-h1260-s-no-gm?authuser=4"
+  },
+  {
+    "year": "2024",
+    "date": "2024.01.16",
+    "title": "HQ-2024.01.16 凱斯總部1月教師訓練",
+    "link": "https://photos.google.com/u/4/share/AF1QipPsyRF3ER82qnEbBSPK7qUd1wnUqIbJjt8C2weeiZkcVRbor4kFZiUP-IyRqxceDg?key=WENIdy1BNzdpeGxFQ2J6WEQydE8tVnVYTjg4bVRn",
+    "img": "https://lh3.googleusercontent.com/pw/AP1GczMs0jw_A9uDM1leJZ43avZEkUQc16c_4DNC_CnFjaouPPqRHh5BHjxQLupJuJlT7UZAxeKkhqCM2fJs9nW5HhW9dw3FXya2jT6n58QiHA4RNQdAO_S2MRbEUvl-NO_TyM2eNj61j4vT9BuVULWkOcLPoQ=w1890-h1260-s-no-gm?authuser=4"
+  }
+];
